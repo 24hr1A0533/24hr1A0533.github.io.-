429df941a56ba8b4294de59c653d9829679f4522
24hr1A0533 <kingsofmcg@gmail.com>
2026-06-12 14:59:23 +0530

    nothing

---
+++ Maniplation.js
@@ -0,0 +1,3 @@
+let greet = document.getElementsByTagName("h1");
+console.log(greet[0].innerText="Happy Birthday Darling"); 
+
---
+++ input.js
@@ -0,0 +1,13 @@
+function greet_afternoon(){
+    let wishes=[""]
+    let text=document.getElementById("greet");
+    text.innerHTML="good afternoon";
+    let light=document.getElementById("off");
+    light.src="files/image (1).png";
+}
+function change_color(){
+    let color=document.getElementById("cap");
+    color.style.background="black";
+    let txt1=document.getElementById("cap");
+    color.style.color="white";
+}
